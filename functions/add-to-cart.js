9e34ca5a8dd3613d5e54ac733b8805098fd9c80d
Mohammed1DZ <mohammedfreehali@gmail.com>
2025-02-03 13:57:00 +0100

    Delete functions directory

--- functions/add-to-cart.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-  const data = JSON.parse(event.body);
-  // Process data here
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Added to cart!" })
-  };
-};
